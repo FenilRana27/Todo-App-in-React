d130ed6aac84c03fa74b5c62ae83511907f7f9f3
Fenil Rana <fenilrana24@gmail.com>
2026-01-01 12:43:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,46 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task !== "") {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="container">
+      <h2>Todo App</h2>
+
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
 
